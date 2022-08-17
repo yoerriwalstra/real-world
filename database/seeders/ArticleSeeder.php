5711f74b7d21acb0ec13769931960c23ae20ba8a
yoeriwalstra <yoeriwalstra@sterrkdeta.nl>
2022-08-17 17:10:12 +0200

    add articles table

---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = User::all();
+        $users->each(function (User $user) {
+            Article::factory(3)
+                ->for($user, 'author')
+                ->create();
+        });
+    }
+}
