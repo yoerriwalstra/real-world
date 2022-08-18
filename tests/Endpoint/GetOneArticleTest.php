83448392cec3ffe85d33f14bae76a813180c4972
yoeriwalstra <yoeriwalstra@sterrkdeta.nl>
2022-08-18 09:40:50 +0200

    add tests to get one article

---
+++ tests/Endpoint/GetOneArticleTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Endpoint;
+
+use App\Models\Article;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class GetOneArticleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public Article $article;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testItThrowsModelNotFoundException()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->expectException(ModelNotFoundException::class);
+
+        $this->getJson('/api/articles/nonExistentArticle');
+    }
+
+    public function testItReturnsNotFound()
+    {
+        $response = $this->getJson('/api/articles/nonExistentArticle');
+
+        $response->assertNotFound();
+        $response->assertJson(['message' => 'Article not found']);
+    }
+
+    public function testItReturnsTheArticle()
+    {
+        Carbon::setTestNow(Carbon::now());
+
+        $user = User::factory()->create();
+        $article = Article::factory()
+            ->for($user, 'author')
+            ->create(['title' => 'easy title']);
+
+        $response = $this->getJson("/api/articles/{$article->slug}");
+
+        $response->assertOk();
+        $response->assertJson([
+            'article' => [
+                'slug' => $article->slug,
+                'title' => $article->title,
+                'description' => $article->description,
+                'body' => $article->body,
+                'tagList' => null,
+                'createdAt' => (string) $article->created_at,
+                'updatedAt' => (string) $article->updated_at,
+                'favorited' => null,
+                'favoritesCount' => null,
+                'author' => [
+                    'username' => $user->username,
+                    'bio' => $user->bio,
+                    'image' => $user->image,
+                    'following' => false,
+                ]
+            ]
+        ]);
+    }
+}
